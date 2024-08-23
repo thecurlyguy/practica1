0b146af35bcc5473f808daa09293ee5d6fd36157
thecurlyguy <serchio31926@gmail.com>
2024-08-23 01:25:23 -0600

    Java with Maven

---
+++ POOP1_ANT/src/poop1_ant/POOP1_ANT.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop1_ant;
+
+/**
+ *
+ * @author SERGIO
+ */
+public class POOP1_ANT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo con ANT");
+        System.out.println("Desde POO G3");
+        int a = 3;
+        Integer b=3;
+        String cadena = new String("hola");
+        System.out.println(a);//parseo
+        System.out.println(b);//parseo
+        System.out.println(cadena);//ninguna conversion
+        
+    }
+    
+}
---
+++ POOP1_MAVEN/src/main/java/com/mycompany/poop1_maven/POOP1_MAVEN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poop1_maven;
+
+/**
+ *
+ * @author SERGIO
+ */
+public class POOP1_MAVEN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
